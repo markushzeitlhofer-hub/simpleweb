84a982e4051d9e1730b9c189e4940860fe50fbb1
dresanovic <daniel.resanovic@gmail.com>
2025-10-06 12:24:43 +0200

    adding lint to pockage.json to fix the broken run

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi there');
+    res.send('hello world');
 });
 
 // Only listen when started directly: `node index.js`
